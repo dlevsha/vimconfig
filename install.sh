ea77674f1a95894b1ca8de7b07b3f3dbdb4f123f
Dmitry Levsha <dlevsha@gmail.com>
2021-06-02 14:36:02 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,33 +1,4 @@
-if [ ! -d ~/.vim ]; then
-    mkdir ~/.vim
-fi
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.vimrc https://raw.githubusercontent.com/dlevsha/vimconfig/master/.vimrc
 
-if [ ! -d ~/.vim/autoload ]; then
-    mkdir ~/.vim/autoload
-fi
-
-if [ ! -f ~/.vim/autoload/pathogen.vim ]; then
-    cd ~/.vim/autoload
-    wget https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
-fi
-
-if [ ! -d ~/.vim/bundle ]; then
-    mkdir ~/.vim/bundle
-fi    
-cd ~/.vim/bundle
-git clone https://github.com/scrooloose/nerdtree.git
-git clone https://github.com/kien/ctrlp.vim.git
-git clone git://github.com/altercation/vim-colors-solarized.git
-git clone https://github.com/kchmck/vim-coffee-script.git
-git clone git://github.com/shawncplus/phpcomplete.vim.git
-git clone https://github.com/vim-scripts/taglist.vim.git
-git clone https://github.com/mattn/emmet-vim.git
-git clone https://github.com/Lokaltog/vim-easymotion
-git clone https://github.com/scrooloose/syntastic.git
-git clone git://github.com/ervandew/supertab.git
-git clone git://github.com/tpope/vim-surround.git
-
-if [ -f ~/.vimrc ]; then
-    mv ~/.vimrc ~/.vimrc.backup
-fi
-cp ~/vimconfig/.vimrc ~/.vimrc
+echo "Open vim and run :PlugInstall"
